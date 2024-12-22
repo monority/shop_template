865aaa65f720855f371769b0e449511a0b12cb45
monority <chenu.pro@gmail.com>
2024-12-22 19:44:58 +0100

    NEW subnav and hero page

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,19 +1,36 @@
 import React from 'react'
 import { useLocation } from 'react-router';
 
-const Hero = ({ video }) => {
+const Hero = ({ video, img }) => {
 	const location = useLocation();
 	const checklocation = location.pathname === "";
 	video = "/video/hero.webm"
+	img = "/hero.png"
 	return (
 		<>
-
-				<div id='hero'>
+			<div className="lyt_container">
+				<section id="hero">
 					<div className="container">
-						{/* <video src={video} autoPlay loop muted></video> */}
+						{/* <div className="video_container">
+						<div className="video_figure">
+							<video src={video} autoPlay loop muted></video>
+						</div>
+					</div> */}
+						<div className="img_container">
+							<div className="img_figure">
+								<img src={img} alt="" />
+							</div>
+						</div>
+						<div className="content_container">
+							<div className="wrapper">
+								<h1>We strive to give you the sneaker you looking for</h1>
+							</div>
+						</div>
 					</div>
-				</div>
-			
+				</section>
+			</div>
+
+
 
 		</>
 	)
