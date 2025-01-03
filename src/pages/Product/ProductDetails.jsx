fb0112939fb9e26c37aea19ec80ce92bd77b41d2
monority <chenu.pro@gmail.com>
2025-01-03 16:48:25 +0100

    NEW : New array for size US, need to translate in EU Size

--- src/pages/Product/ProductDetails.jsx
+++ src/pages/Product/ProductDetails.jsx
@@ -3,18 +3,31 @@ import { useParams } from 'react-router'
 import { ProductById } from '../../queries/ProductFetch';
 const ProductDetails = () => {
 	const { id } = useParams();
-	const [data, setData] = useState();
+	const [data, setData] = useState(null);
+	const [variantSize, setVariantSize] = useState([]);
+	const [stockSize, setStockSize] = useState([]);
+  
 	useEffect(() => {
-		ProductById(setData, id)
-		console.log(data)
-	}, []);
+	  ProductById(setData, id);
+	}, [id]);
+  
+	useEffect(() => {
+	  if (data && data.data && data.data.variants) {
+		setVariantSize(data.data.variants);
+		const sizes = data.data.variants.map(variant => variant.size);
+		setStockSize(sizes);
+		console.log(stockSize)
+	  }
+	}, [data]);
+  
+	
 	return (
 		<>
 			<div id="product_details">
 				<div className="lyt_container">
 					<div className="container">
 						<div className="wrapper_path">
-							<p>{data?.data?.category} {">"} {data?.data?.brand}</p> 
+							<p>{data?.data?.category} {">"} {data?.data?.brand}</p>
 						</div>
 						<div className="container_content">
 							<div className="wrapper_image">
@@ -31,10 +44,25 @@ const ProductDetails = () => {
 								<div className="element">
 									<p>Reviews : </p>
 								</div>
+								<div className="element">
+									<h1>{data?.data?.avg_price} $</h1>
+								</div>
+								<div className="element_list">
+									<>
+										{data?.data?.color?.split('/').map((color, index) => (
+										<p key={index} className={`${!index ? "active" : ""} default_btn`}>{color}</p>
+										))}	
+									</>
+								</div>
+								<div className="element_list">
+									<>
+										{stockSize.sort()}
+									</>
+								</div>
 							</div>
 						</div>
 					</div>
-					
+
 				</div>
 			</div>
 		</>
