b37f3f1c955fb7d35e0adf6ddd74d1275770267c
Ronan C <126070278+monority@users.noreply.github.com>
2025-01-08 15:59:45 +0100

    FIX : Fixing CSS newsletter position image bg

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import Root from './Root'
 import './assets/sass/main.scss'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Root />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <Root />
+  </React.StrictMode>,
 )
